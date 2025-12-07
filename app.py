09a10300a8c5a6899ada337b462c40482d7cd36d
Narayani Rajput <narayaniarajput@gmail.com>
2025-12-07 15:38:52 +0530

    Commit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates", static_folder="static")
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///finance.db"
 db = SQLAlchemy(app)
 
